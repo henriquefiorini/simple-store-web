4efdebe0aa5b5524b0730d66ff3839c0850572a4
Henrique Fiorini <henriquehfr2@gmail.com>
2019-09-09 12:45:11 -0400

    Initial configurations

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+
+    'import/prefer-default-export': 'off',
+
+    'react/jsx-filename-extension': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p>This is a React + Redux project.</p>
+    </>
+  );
+}
+
+export default App;
